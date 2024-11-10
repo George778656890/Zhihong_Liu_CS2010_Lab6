b48e3e08e4b506c07803a2f9b8563643dcd45a5e
ZHIHONG LIU <778656890@qq.com>
2024-11-09 20:24:04 -0700

    Add project files.

---
+++ Zhihong_Liu_CS2010_Lab6/Zhihong_Liu_CS2010_Lab6.cpp
@@ -0,0 +1,111 @@
+﻿//Memory management simulation
+#include<utility>
+#include <iostream>
+#include<vector>
+#include <ctime> 
+#include <cstdlib> 
+#include<algorithm>
+#include <numeric>
+#include"zhihong_liu.h"
+
+int main()
+{
+	std::srand(static_cast<unsigned int>(std::time(0)));//Seed value
+
+	long int clock = 0;
+
+	freelist_t freeList{ range(0,MEMORY_SIZE) };
+	alloclist_t allocList;
+
+
+
+	do {
+		//Generate a memory request
+		if (!(clock % REQUEST_INTERVAL)) {
+
+			++totalRequest;//Statistical variable
+
+
+			int blockSize = rand() % (MAX_SIZE - MIN_SIZE) + MIN_SIZE;
+			int leaseDuration = rand() % (MAX_LEASE - MIN_LEASE) + MIN_LEASE;
+			int leaseExpiry = clock + leaseDuration;
+
+			generatedBlockSize.push_back(blockSize);        //Statistical variable
+			generatedLeaseDuration.push_back(leaseDuration);//Statistical variable
+
+		RetryAfterMerging:
+
+			int NthRangeObject = -1;//Mark which Range object in the freeList is selected
+
+			int assignedIndex = searchSuitableSizeInFreeList(freeList, blockSize, &NthRangeObject);
+
+			if ((assignedIndex != -1) && (NthRangeObject != -1)) {
+
+				++satisfiedRequest;//Statistical variable
+
+				allocList.push_back(alloc(range(assignedIndex, blockSize), leaseExpiry));//Push the "alloc object" into the "allocList"
+
+				if (freeList[NthRangeObject].second == blockSize) {//Size matches perfectly,remove the corresponding "Range Object" from "freeList"
+
+					freeList.erase(freeList.begin() + NthRangeObject);
+				}
+				else
+				{
+					adjustRange(freeList[NthRangeObject], blockSize);  //Adjust the selected "Range Object"
+				}
+
+			}
+			else {//Merging
+				if (merge(freeList, blockSize)) {
+					goto RetryAfterMerging;
+				}
+				else {//Merge fails
+					++unSatisfiedRequest;//Statistical variable
+				}
+
+
+
+			}
+
+		}
+
+
+		checkLeaseExpiry(freeList, allocList, clock);
+
+		sort(freeList.begin(), freeList.end(), comp1);
+		sort(allocList.begin(), allocList.end(), comp2);
+
+	} while (++clock < TIME_LIMIT);
+
+
+	smallestBlockSize = *std::min_element(generatedBlockSize.begin(), generatedBlockSize.end());
+	largestBlockSize = *std::max_element(generatedBlockSize.begin(), generatedBlockSize.end());
+	averageBlockSize = (accumulate(generatedBlockSize.begin(), generatedBlockSize.end(), 0)) / generatedBlockSize.size();
+
+
+	shortestLeaseDuration = *std::min_element(generatedLeaseDuration.begin(), generatedLeaseDuration.end());
+	longestLeaseDuration = *std::max_element(generatedLeaseDuration.begin(), generatedLeaseDuration.end());
+	averageLeaseDuration = (accumulate(generatedLeaseDuration.begin(), generatedLeaseDuration.end(), 0)) / generatedLeaseDuration.size();
+
+	cout << "The total number of requests is       : " << totalRequest << endl;
+	cout << "The number of satisfied requests is   : " << satisfiedRequest << endl;
+	cout << "The number of unsatisfied requests is : " << unSatisfiedRequest << "\n\n";
+
+	cout << "The smallest block size is            : " << smallestBlockSize << endl;
+	cout << "The largest block size is             : " << largestBlockSize << endl;
+	cout << "The average block size is             : " << averageBlockSize << "\n\n";
+
+	cout << "The shortest lease duration is        : " << shortestLeaseDuration << endl;
+	cout << "The longest lease duration is         : " << longestLeaseDuration << endl;
+	cout << "The average lease duration is         : " << averageLeaseDuration << "\n\n";
+
+	cout << "The number of times of merging is     : " << mergingTimes << "\n\n";
+
+
+	printFree(freeList);
+	cout << "\n" << string(120, '*') << "\n" << endl;
+	printAlloc(allocList);
+
+
+	return 0;
+}
---
+++ Zhihong_Liu_CS2010_Lab6/zhihong_liu.h
@@ -0,0 +1,110 @@
+#ifndef zhihong_liu_h
+#define zhihong_liu_h
+
+
+using namespace std;
+
+using range = pair<int, int>;
+using freelist_t = vector<range>;
+using alloc = pair<range, int>;
+using alloclist_t = vector<alloc>;
+
+const int MIN_LEASE = 40;
+const int MAX_LEASE = 70;
+const int MIN_SIZE = 50;
+const int MAX_SIZE = 350;
+const int TIME_LIMIT = 1000;
+const int MEMORY_SIZE = 1000;
+const int REQUEST_INTERVAL = 10;
+
+int mergingTimes = 0;
+
+int satisfiedRequest = 0;
+int unSatisfiedRequest = 0;
+int totalRequest = 0;
+
+int smallestBlockSize = 0;
+int largestBlockSize = 0;
+int averageBlockSize = 0;
+vector<int> generatedBlockSize;
+
+
+int shortestLeaseDuration = 0;
+int longestLeaseDuration = 0;
+int averageLeaseDuration = 0;
+vector<int> generatedLeaseDuration;
+
+
+
+void printFree(freelist_t& f) {
+	cout << "Free List\n";
+	for (auto e : f) {
+		auto [start, size] = e;
+		cout << "[ " << start << "," << size << " ]";
+	}
+	cout << endl;
+}
+
+void printAlloc(alloclist_t& a) {
+	cout << "Alloc List\n";
+	for (auto e : a) {
+		auto [r, lease] = e;
+		auto [start, size] = r;
+		cout << "[ ( " << start << "," << size << ")," << lease << " ) ]";
+	}
+	cout << endl;
+}
+
+bool comp1(const range& a, const range& b) {//Compare the start index
+	return a.first < b.first;  //Ascending
+}
+
+bool comp2(const alloc& a, const alloc& b) {//Compare the lease time
+	return a.second < b.second; //Ascending
+}
+
+void checkLeaseExpiry(freelist_t& f, alloclist_t& a, int clock) {
+	for (int i = 0; i < a.size(); ++i) {
+		if (a[i].second == clock) {
+			f.push_back(a[i].first);//Give back the memory to freeList
+			a.erase(a.begin() + i);//Remove the expired "alloc object" from the "allocList"
+		}
+	}
+}
+
+//Use the random-generated "blockSize" to search in the freeList,return the index
+int searchSuitableSizeInFreeList(freelist_t& f, int blockSize, int* NthRangeObject) {
+
+	for (int i = 0; i < f.size(); ++i) {
+		if (blockSize <= f[i].second) {
+			*NthRangeObject = i;//Mark which Range object in the freeList is selected(function couldn't have two return values)
+			return f[i].first;
+
+		}
+	}
+
+	return -1;//No suitable size is found
+}
+
+//Adjust the "Range object in the freeList" after part of which is allocated to the "allocList"
+void adjustRange(range& r, int blockSize) {
+	r.first = r.first + blockSize;
+	r.second = r.second - blockSize;
+}
+
+bool merge(freelist_t& f, int expectedSize) {
+
+	++mergingTimes;//Statistical variable
+
+	for (int i = 0; i < f.size() - 1; ++i) {
+		if (((f[i].first + f[i].second) == f[i + 1].first) && ((f[i].second + f[i + 1].second) >= expectedSize)) {//Check adjacency and size
+			f[i].second = f[i].second + f[i + 1].second;//merging by changing the size of the current "range object"
+			f.erase(f.begin() + (i + 1)); // Remove the next range object which has been merged
+			return true;
+		}
+	}
+	return false;
+}
+
+
+#endif
